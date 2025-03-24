04f2662d1157134d41fdde020a8bf9b7f85d4711
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-03-24 14:24:31 +0530

    Time: 3 ms (48.89%), Space: 20 MB (44.46%) - LeetHub

--- 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
+++ 0637-average-of-levels-in-binary-tree/0637-average-of-levels-in-binary-tree.py
@@ -6,19 +6,41 @@
 #         self.right = right
 class Solution:
     def averageOfLevels(self, root: Optional[TreeNode]) -> List[float]:
+        # res=[]
+        # queue=deque([root])
+        # while queue:
+        #     sum1=0
+        #     c=len(queue)
+        #     for  i in range(len(queue)):
+        #         node=queue.popleft()
+        #         sum1+=node.val
+        #         if node.left:
+        #             queue.append(node.left)
+        #         if node.right:
+        #             queue.append(node.right)
+        #     res.append(sum1/c)
+        # return res
+
+        
+   #dfs soln------------
+        s=[]
+        c=[]
+        # node=root(1)
+        # depth=0
+        def dfs(root,depth) :
+            if not root:
+                return 
+            if depth<len(s):
+                s[depth]+=root.val
+                c[depth]+=1
+            else:
+                s.append(root.val)
+                c.append(1)
+            dfs(root.left,depth+1)
+            dfs(root.right,depth+1)
+        dfs(root,0)
         res=[]
-        queue=deque([root])
-        while queue:
-            sum1=0
-            c=len(queue)
-            for  i in range(len(queue)):
-                node=queue.popleft()
-                sum1+=node.val
-                if node.left:
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            res.append(sum1/c)
+        for i in range(len(s)):
+            avg=s[i]/c[i]
+            res.append(avg)
         return res
-        
-        
