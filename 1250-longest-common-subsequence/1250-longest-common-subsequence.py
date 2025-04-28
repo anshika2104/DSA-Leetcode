c68123eeab15135845bcf8c950b5e74fb3a65e64
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-04-28 14:51:29 +0530

    Time: 430 ms (79.94%), Space: 42.8 MB (71.92%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.py
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.py
@@ -1,5 +1,6 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        # -----------------------------recursion----------------------------
         # def rec(i,j):
         #     if i==len(text1) or j==len(text2):
         #         return 0
@@ -8,17 +9,29 @@ class Solution:
         #     else:
         #         return max(rec(i+1,j),rec(i,j+1))
         # return rec(0,0)
-        # memoization
-        memo={}
-        def rec(i,j):
-            if i==len(text1) or j==len(text2):
-                return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
-            if text1[i]==text2[j]:
-                return (1+rec(i+1,j+1))
-            else:
-                memo[(i,j)]= max(rec(i+1,j),rec(i,j+1))
-                return memo[(i,j)]
-        return rec(0,0)
+
+
+        # ----------------------memoization-----------------------------------------------
+        # memo={}
+        # def rec(i,j):
+        #     if i==len(text1) or j==len(text2):
+        #         return 0
+        #     if (i,j) in memo:
+        #         return memo[(i,j)]
+        #     if text1[i]==text2[j]:
+        #         return (1+rec(i+1,j+1))
+        #     else:
+        #         memo[(i,j)]= max(rec(i+1,j),rec(i,j+1))
+        #         return memo[(i,j)]
+        # return rec(0,0)
+        # ---tabulation--------------------
+        m,n=len(text1),len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(0,m):
+            for j in range(0,n):
+                if text1[i]==text2[j]:
+                    dp[i+1][j+1]=1+dp[i][j]
+                else:
+                    dp[i+1][j+1]=max(dp[i][j+1],dp[i+1][j])
+        return dp[m][n]
 
