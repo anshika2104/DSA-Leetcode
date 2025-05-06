8a5955f1a483d5458629d2184465b9ae0022b88c
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-05-06 15:00:35 +0530

    Time: 57 ms (98.41%), Space: 15.4 MB (17.01%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -4,19 +4,22 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        f={}
-        for i in nums:
-            if i in f:
-                f[i]+=1
-            else:
-                f[i]=1
-        for key in list(f.keys()):  # Iterate over keys instead of values
-            if f[key] > 2:  # Check frequency using correct reference
-                while nums.count(key) > 2:  # Remove extra occurrences
-                    nums.remove(key)  # Remove one occurrence at a time
-        
-        return len(nums)
-        # for key,val in f.items():
-        #     nums[:] = [key for key in nums if f[key] <= 2]
+        # f={}
+        # for i in nums:
+        #     if i in f:
+        #         f[i]+=1
+        #     else:
+        #         f[i]=1
+        # for key in list(f.keys()):  
+        #     if f[key] > 2:
+        #         while nums.count(key) > 2:  
+        #             nums.remove(key)  
         # return len(nums)
+        k=2
+        for i in range(2,len(nums)):
+            if nums[i]!=nums[k-2]:
+                nums[k]=nums[i]
+                k+=1
+        return k
+       
         
