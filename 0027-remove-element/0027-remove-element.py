2ef129296e6508b652c5c626323781a5f50fd18b
anshika2104 <91836420+anshika2104@users.noreply.github.com>
2025-05-06 12:29:48 +0530

    Time: 0 ms (100%), Space: 12.6 MB (9.67%) - LeetHub

--- 0027-remove-element/0027-remove-element.py
+++ 0027-remove-element/0027-remove-element.py
@@ -1,20 +1,12 @@
-class Solution:
-    def removeElement(self, nums: List[int], val: int) -> int:
-        # using remove fxn ------------------------------
-        # c=nums.count(val)
-        # for i in range(c):
-        #     nums.remove(val)
-        # return len(nums)
-        # using list comprehension--------------
-        # nums[:]=[i for i in nums if i !=val]
-        # return len(nums)
-        # ----------two pointer----
-        i,j=0,0
-        while i<len(nums): 
-            if nums[i]!=val:
-                nums[j]=nums[i]
-                j+=1
-            i+=1
-        return j
-        
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        while val in nums:
+            nums.remove(val)
+        return len(nums)
+
         
